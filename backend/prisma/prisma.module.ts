6272067a4e4708b18e2aed909a154894d9202fb6
KikoEnakTau <theo.niomba@gmail.com>
2026-02-13 15:37:52 +0700

    patch: prisma configuration

--- backend/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // Membuatnya bisa dipakai di mana saja tanpa import ulang
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
